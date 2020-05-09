2fb0f417d1a2c8f251bf774a18b7ae4eeee8277e
ryoshid <r.yoshida.c@gmail.com>
2020-05-09 15:46:21 +0900

    Fix webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -19,13 +19,7 @@ module.exports = {
       },
       {
         test: /\.css/,
-        use: [
-          "style-loader",
-          {
-            loader: "css-loader",
-            options: { url: false },
-          },
-        ],
+        use: ["style-loader", "css-loader"],
       },
     ],
   },
