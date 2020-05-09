bf088d2890e1ef0e5dcdb6a2de8bed08b42cc2ae
ryoshid <r.yoshida.c@gmail.com>
2020-05-09 16:51:56 +0900

    Add preview window
    remark and remark-react don't have enough type support. There is an undefined
    property `result` and I had to use `any`. Consider use other packages
    e.g. marked.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import React, { useState } from "react";
+import CSS from "csstype";
 import { Controlled as CodeMirror } from "react-codemirror2";
 import "codemirror/mode/gfm/gfm";
 import "codemirror/lib/codemirror.css";
-import "codemirror/theme/solarized.css";
+import remark from "remark";
+const remarkReact = require("remark-react");
+
+const processor = remark().use(remarkReact);
 
 const App: React.FC = () => {
   const [val, setVal] = useState("");
@@ -10,21 +14,41 @@ const App: React.FC = () => {
     mode: "gfm",
     lineWrapping: true,
     lineNumbers: true,
-    theme: "solarized light",
+    theme: "default",
+  };
+  const compileMd = (mkd: string) => {
+    const file: any = processor.processSync(mkd);
+    return file.result || file.content;
   };
   return (
-    <div style={{ width: "50%" }}>
+    <>
       <h1>App</h1>
-      <CodeMirror
-        value={val}
-        options={options}
-        onBeforeChange={(editor, data, value) => {
-          setVal(value);
-        }}
-        onChange={(editor, data, value) => {}}
-      />
-    </div>
+      <div style={{ display: "flex" }}>
+        <div style={styles.editor}>
+          <CodeMirror
+            className="editor"
+            value={val}
+            options={options}
+            onBeforeChange={(editor, data, value) => {
+              setVal(value);
+            }}
+            onChange={(editor, data, value) => {}}
+          />
+        </div>
+        <div style={styles.preview}>{compileMd(val)}</div>
+      </div>
+    </>
   );
+};
+
+const styles: { [name: string]: CSS.Properties } = {
+  editor: {
+    width: "50%",
+  },
+  preview: {
+    width: "50%",
+    backgroundColor: "#d0d0d0",
+  },
 };
 
 export default App;
